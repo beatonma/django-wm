c7ec1f013803f7c4ef6c236b4ef0085ba53225ad
Michael Beaton <beatonma@gmail.com>
2018-04-25 20:59:49 +0100

    Removed old commented code

--- tasks/incoming_webmentions.py
+++ tasks/incoming_webmentions.py
@@ -107,56 +107,6 @@ def _get_target_object(target_url):
         raise(e)
 
 
-# def _get_model_for_url(target_path):
-#     """
-#     Find a match in urlpatterns and return the corresponding model instance.
-#     """
-#     target_path = target_path.lstrip('/')  # Remove any leading slashes
-#     log.info('Looking for object to match url {}'.format(target_path))
-#     urlconf = import_module(settings.ROOT_URLCONF)
-#     urlpatterns = urlconf.urlpatterns
-#     for x in urlpatterns:
-#         # x may be an instance of either:
-#         # - django.urls.resolvers.URLResolver
-#         # - django.urls.resolvers.URLPattern
-#         try:
-#             match = x.resolve(target_path)
-#             if match:
-#                 break
-#         except:
-#             pass
-#     else:
-#         # No match found
-#         raise TargetDoesNotExist('Cannot find a matching urlpattern entry')
-
-#     log.info('Found matching urlpattern: {}'.format(match))
-
-#     # Dotted path to model class declaration
-#     model_name = match.kwargs.get('model_name')
-
-#     # Required to retrieve the target model instance
-#     slug = match.kwargs.get('slug')
-
-#     if not model_name:
-#         raise BadConfig(
-#             'urlpattern must include a kwarg entry called \'model_name\': {}'
-#             .format(match))
-#     if not slug:
-#         raise BadConfig(
-#             'urlpattern must include a kwarg entry called \'slug\': {}'
-#             .format(match))
-
-#     from django.apps import apps
-#     model = apps.get_model(model_name)
-
-#     try:
-#         return model.objects.get(slug=slug)
-#     except ObjectDoesNotExist:
-#         raise TargetDoesNotExist(
-#             'Cannot find instance of model=\'{}\' with slug=\'{}\''
-#             .format(model, slug))
-
-
 def _get_incoming_source(source_url):
     """
     Fetch the source, confirm content is suitable and return response.
