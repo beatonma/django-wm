a23a7723d5653639e8905242179050ef2c44a62a
Michael Beaton <beatonma@gmail.com>
2019-03-11 21:58:47 +0000

    Use stub model for testing instead of importing from external app.

--- tests/test_webmentions_tasks.py
+++ tests/test_webmentions_tasks.py
@@ -1,23 +1,43 @@
 import logging
 
+from django.conf import settings
 from django.test import TestCase
 
-from main.models import App
-from mentions.tasks import webmentions as wm
+from django.urls import re_path
 
+from mentions.util import get_model_for_url
+from mentions.views import GetWebmentionsView, WebmentionView
+
+from mentions.models.test import MentionableTestStub
+from mentions.urls import urlpatterns
+from mentions.views.test import MentionableTestStubView
 
 log = logging.getLogger(__name__)
+
+urlpatterns += [
+    re_path(
+        r'^mentionable_test_stub/(?P<slug>[\w\-.]+)/?$',
+        MentionableTestStubView.as_view(),
+        kwargs={
+            'model_name': 'mentions.MentionableTestStub',
+        },
+        name='mentionable_test_stub_view'),
+]
 
 
 class WebmentionTasksTestCase(TestCase):
     def setUp(self):
-        App.objects.create(
-            title='SCII Achievements Manager',
-            slug='scii-achievements-manager',
-            app_id='sc2am')
+        self.stub_id = 'some-id123'
+        self.slug = 'some-slug'
+
+        MentionableTestStub.objects.create(
+            stub_id=self.stub_id,
+            slug=self.slug).save()
 
     def test_get_model_for_url(self):
-        obj = wm._get_model_for_url('app/scii-achievements-manager')
-        log.info('model: {}'.format(obj))
+        '''Ensure that reverse url lookup finds the correct object.'''
+        retreived_object = get_model_for_url(
+            f'/webmention/mentionable_test_stub/{self.slug}')
+        log.info(f'model: {retreived_object}')
 
-        self.assertEqual(obj.title, 'SCII Achievements Manager')
+        self.assertEqual(retreived_object.stub_id, self.stub_id)
