b9bc4348514d5aaef90890d14aa337d99b40e92f
Michael Beaton <beatonma@gmail.com>
2019-03-17 12:37:32 +0000

    ''' -> """ pep

--- models/test.py
+++ models/test.py
@@ -8,9 +8,9 @@ log = logging.getLogger(__name__)
 
 
 class MentionableTestStub(MentionableMixin, models.Model):
-    '''
+    """
     Stub model for testing.
-    '''
+    """
 
     stub_id = models.CharField(max_length=10)
     slug = models.SlugField(unique=True, max_length=10)
