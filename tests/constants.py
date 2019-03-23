7fdb6588dede564f07f8927318ecab101a11a568
Michael Beaton <beatonma@gmail.com>
2019-03-23 22:54:53 +0000

    Refactored view names to a data class to improve autocomplete.

--- tests/constants.py
+++ tests/constants.py
@@ -1,13 +1,30 @@
 # Base URL path (global urlpatterns)
+from dataclasses import dataclass
+
 namespace = 'webmention'
 
 model_name = 'tests.MentionableTestModel'
 
-unmentionable_view = 'unmentionable_view'
-http_header_view = 'http_header_view'
-html_head_view = 'html_head_view'
-html_anchor_view = 'html_anchor_view'
-all_endpoints_view = 'all_endpoints_view'
+
+@dataclass
+class Views:
+    unmentionable = 'unmentionable_view'
+    http_header = 'http_header_view'
+    html_head = 'html_head_view'
+    html_anchor = 'html_anchor_view'
+    all_endpoints = 'all_endpoints_view'
+
+
+views = Views()
+
+
+# views = {
+#     'unmentionable': 'unmentionable_view',
+#     'http_header': 'http_header_view',
+#     'html_head': 'html_head_view',
+#     'html_anchor': 'html_anchor_view',
+#     'all_endpoints': 'all_endpoints_view',
+# }
 
 # Path as configured in the local urlpatterns
 correct_config = 'with_correct_config'
