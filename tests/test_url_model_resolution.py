8d0c9884103108603fec62cc7a19eb419a0998d0
Michael Beaton <beatonma@gmail.com>
2019-03-23 22:57:10 +0000

    Added view parameter to url() to allow testing of different views

--- tests/test_url_model_resolution.py
+++ tests/test_url_model_resolution.py
@@ -5,57 +5,60 @@ from django.urls import re_path
 
 from mentions.exceptions import BadConfig, TargetDoesNotExist
 from mentions.tests import util
-from mentions.tests.models import MentionableTestStub
-from mentions.tests.views.mentionable import MentionableTestStubView
+from mentions.tests.models import MentionableTestModel
+from mentions.tests.views.mentionable import AllEndpointsMentionableTestView
+from mentions.tests import constants
+from mentions.tests.constants import views
 from mentions.urls import urlpatterns
 from mentions.util import get_model_for_url
 
 log = logging.getLogger(__name__)
-
 
 # Paths as configured in the local urlpatterns
 bad_modelname_key = 'with_bad_kwarg_key'
 bad_modelname_value = 'with_bad_kwarg_value'
 bad_path = 'some_bad_path'
 
-
 urlpatterns += util.correct_urlpatterns + [
     re_path(
-        fr'^{bad_modelname_key}/(?P<slug>[\w\-.]+)/?$',
-        MentionableTestStubView.as_view(),
+        fr'^{bad_modelname_key}/{views.all_endpoints}/'
+        fr'(?P<slug>[\w\-.]+)/?$',
+        AllEndpointsMentionableTestView.as_view(),
         kwargs={
             'model_name_with_mistyped_key': 'tests.MentionableTestStub',
         },
-        name='mentionable_test_stub_view'),
+        name=f'bad_modelname_key_{views.all_endpoints}'),
     re_path(
-        fr'^{bad_modelname_value}/(?P<slug>[\w\-.]+)/?$',
-        MentionableTestStubView.as_view(),
+        fr'^{bad_modelname_value}/{views.all_endpoints}/'
+        fr'(?P<slug>[\w\-.]+)/?$',
+        AllEndpointsMentionableTestView.as_view(),
         kwargs={
             'model_name': 'tests.UnresolvableModel',
         },
-        name='mentionable_test_stub_view'),
+        name=f'bad_modelname_value_{views.all_endpoints}'),
 ]
 
 
 class GetModelForUrlTests(TestCase):
     """Tests for util.get_model_for_url."""
+
     def setUp(self):
         self.stub_id = 'some-id123'
         self.slug = 'some-slug'
 
-        MentionableTestStub.objects.create(
+        MentionableTestModel.objects.create(
             stub_id=self.stub_id,
             slug=self.slug).save()
 
     def test_get_model_for_url__with_unknown_url(self):
         """Ensure that URL with an unrecognised path raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
-            get_model_for_url(util.url(bad_path))
+            get_model_for_url(util.url(views.all_endpoints, bad_path))
 
     def test_get_model_for_url__with_correct_slug(self):
         """Ensure that reverse url lookup finds the correct object."""
         retrieved_object = get_model_for_url(
-            util.url(util.correct_config, self.slug))
+            util.url(constants.correct_config, views.all_endpoints, self.slug))
 
         self.assertEqual(retrieved_object.stub_id, self.stub_id)
 
@@ -63,7 +66,9 @@ class GetModelForUrlTests(TestCase):
         """Ensure that an unknown slug raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
             get_model_for_url(
-                util.url(util.correct_config, 'unknown-slug'))
+                util.url(constants.correct_config,
+                         views.all_endpoints,
+                         'unknown-slug'))
 
     def test_get_model_for_url__with_bad_model_name_config(self):
         """
@@ -75,9 +80,9 @@ class GetModelForUrlTests(TestCase):
         # Raise BadConfig if kwargs does not have a key named `model_name`.
         with self.assertRaises(BadConfig):
             get_model_for_url(
-                util.url(bad_modelname_key, self.slug))
+                util.url(bad_modelname_key, views.all_endpoints, self.slug))
 
         # Raise BadConfig if `model_name` kwarg has bad/unresolvable path.
         with self.assertRaises(BadConfig):
             get_model_for_url(
-                util.url(bad_modelname_value, self.slug))
+                util.url(bad_modelname_value, views.all_endpoints, self.slug))
