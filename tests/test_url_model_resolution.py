fd26b889c853b1e04d0d4985af64a6e7d3a762e6
Michael Beaton <beatonma@gmail.com>
2019-03-23 16:04:39 +0000

    Moved reused urlpatterns and url() to tests.util
    Signed-off-by: Michael Beaton <beatonma@gmail.com>

--- tests/test_url_model_resolution.py
+++ tests/test_url_model_resolution.py
@@ -1,54 +1,37 @@
 import logging
 
-from django.conf import settings
 from django.test import TestCase
-
 from django.urls import re_path
 
 from mentions.exceptions import BadConfig, TargetDoesNotExist
-from mentions.util import get_model_for_url
-from mentions.views import GetWebmentionsView, WebmentionView
-
-from mentions.models.test import MentionableTestStub
+from mentions.tests import util
+from mentions.tests.models import MentionableTestStub
+from mentions.tests.views.mentionable import MentionableTestStubView
 from mentions.urls import urlpatterns
-from mentions.views.test import MentionableTestStubView
+from mentions.util import get_model_for_url
 
 log = logging.getLogger(__name__)
 
-# Base URL path (global urlpatterns)
-namespace = 'webmention'
 
 # Paths as configured in the local urlpatterns
-correct_config = 'with_correct_config'
 bad_modelname_key = 'with_bad_kwarg_key'
 bad_modelname_value = 'with_bad_kwarg_value'
 bad_path = 'some_bad_path'
 
 
-def _url(path, slug=''):
-    return f'/{namespace}/{path}/{slug}'
-
-
-urlpatterns += [
-    re_path(
-        fr'^{correct_config}/(?P<slug>[\w\-.]+)/?$',
-        MentionableTestStubView.as_view(),
-        kwargs={
-            'model_name': 'mentions.MentionableTestStub',
-        },
-        name='mentionable_test_stub_view'),
+urlpatterns += util.correct_urlpatterns + [
     re_path(
         fr'^{bad_modelname_key}/(?P<slug>[\w\-.]+)/?$',
         MentionableTestStubView.as_view(),
         kwargs={
-            'model_name_with_mistyped_key': 'mentions.MentionableTestStub',
+            'model_name_with_mistyped_key': 'tests.MentionableTestStub',
         },
         name='mentionable_test_stub_view'),
     re_path(
         fr'^{bad_modelname_value}/(?P<slug>[\w\-.]+)/?$',
         MentionableTestStubView.as_view(),
         kwargs={
-            'model_name': 'mentions.UnresolvableModel',
+            'model_name': 'tests.UnresolvableModel',
         },
         name='mentionable_test_stub_view'),
 ]
@@ -67,12 +50,12 @@ class GetModelForUrlTests(TestCase):
     def test_get_model_for_url__with_unknown_url(self):
         """Ensure that URL with an unrecognised path raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
-            get_model_for_url(_url(bad_path))
+            get_model_for_url(util.url(bad_path))
 
     def test_get_model_for_url__with_correct_slug(self):
         """Ensure that reverse url lookup finds the correct object."""
         retrieved_object = get_model_for_url(
-            _url(correct_config, self.slug))
+            util.url(util.correct_config, self.slug))
 
         self.assertEqual(retrieved_object.stub_id, self.stub_id)
 
@@ -80,7 +63,7 @@ class GetModelForUrlTests(TestCase):
         """Ensure that an unknown slug raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
             get_model_for_url(
-                _url(correct_config, 'unknown-slug'))
+                util.url(util.correct_config, 'unknown-slug'))
 
     def test_get_model_for_url__with_bad_model_name_config(self):
         """
@@ -92,9 +75,9 @@ class GetModelForUrlTests(TestCase):
         # Raise BadConfig if kwargs does not have a key named `model_name`.
         with self.assertRaises(BadConfig):
             get_model_for_url(
-                _url(bad_modelname_key, self.slug))
+                util.url(bad_modelname_key, self.slug))
 
         # Raise BadConfig if `model_name` kwarg has bad/unresolvable path.
         with self.assertRaises(BadConfig):
             get_model_for_url(
-                _url(bad_modelname_value, self.slug))
+                util.url(bad_modelname_value, self.slug))
