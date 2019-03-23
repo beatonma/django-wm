60cc9329a3528c5a6a6c9a3bc13e9ddb5f6078bf
Michael Beaton <beatonma@gmail.com>
2019-03-23 22:52:53 +0000

    Added content field for testing outgoing mentions

--- tests/models/mentionable.py
+++ tests/models/mentionable.py
@@ -7,8 +7,10 @@ from mentions.models.mixins.mentionable import MentionableMixin
 log = logging.getLogger(__name__)
 
 
-class MentionableTestStub(MentionableMixin, models.Model):
+class MentionableTestModel(MentionableMixin, models.Model):
     """Stub model for testing."""
 
-    stub_id = models.CharField(max_length=10)
-    slug = models.SlugField(unique=True, max_length=10)
+    stub_id = models.CharField(max_length=16)
+    slug = models.SlugField(unique=True, max_length=20)
+
+    content = models.TextField(blank=True, null=True)
