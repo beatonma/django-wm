557077e5e6bde1c21a80c95c501a602ccc292cf8
Michael Beaton <beatonma@gmail.com>
2019-03-30 18:28:45 +0000

    Implemented tests for /webmentions/get endpoint (for retrieving webmentions for a particular url path)

---
+++ tests/test_get_mentions_for_object.py
@@ -0,0 +1,81 @@
+"""
+Make sure we can correctly retrieve Webmentions for a given url/object.
+"""
+
+import json
+import logging
+
+from django.test import TestCase
+
+from mentions.models import Webmention
+from mentions.tests.models import MentionableTestModel
+from . import util
+from .util import constants
+
+log = logging.getLogger(__name__)
+
+
+def _create_quick_webmention(source, target, valid=True, approved=True):
+    wm = Webmention.create(
+        util.build_object_url(source.slug),
+        util.build_object_url(target.slug),
+        sent_by='tests@localhost')
+    wm.validated = valid
+    wm.approved = approved
+    wm.target_object = target
+    wm.save()
+
+
+class WebmentionGetTests(TestCase):
+    def setUp(self):
+        self.target_stub_id, self.target_slug = util.get_id_and_slug()
+
+        target = MentionableTestModel.objects.create(
+            stub_id=self.target_stub_id,
+            slug=self.target_slug,
+            allow_incoming_webmentions=True)
+        target.save()
+        self.target_url = util.build_object_url(self.target_slug)
+
+        self.source_stub_id, self.source_slug = util.get_id_and_slug()
+        source = MentionableTestModel.objects.create(
+            stub_id=self.source_stub_id,
+            slug=self.source_slug,
+            content=util.get_mentioning_content(self.target_url))
+        source.save()
+        self.source_url = util.build_object_url(self.source_slug)
+
+        _create_quick_webmention(source, target)
+
+    def test_get_webmentions_view__with_valid_object(self):
+        """Ensure that we can correctly retrieve webmentions for a given url."""
+        response = self.client.get(
+            constants.webmention_api_get_relative_url,
+            data={'url': self.target_url})
+
+        json_response = json.loads(response.content)
+        log.info(json_response)
+        self.assertEqual(json_response['status'], 1)
+
+        mentions = json_response['mentions']
+        self.assertEqual(len(mentions), 1)
+
+        first_mention = mentions[0]
+        self.assertEqual(first_mention['source_url'], self.source_url)
+
+
+class WebmentionGetBadRequestTests(TestCase):
+    def test_get_webmentions_view__require_http_get(self):
+        """Ensure that webmentions get view does not accept HTTP POST."""
+        response = self.client.post(constants.webmention_api_get_relative_url)
+        self.assertEqual(405, response.status_code)
+
+    def test_get_webmentions_view__require_param_for_url(self):
+        response = self.client.get(constants.webmention_api_get_relative_url)
+        self.assertEqual(400, response.status_code)
+
+    def test_get_webmentions_view__target_does_not_exist(self):
+        response = self.client.get(
+            constants.webmention_api_get_relative_url,
+            data={'url': '/does-not-exist'})
+        self.assertEqual(json.loads(response.content)['status'], 0)
