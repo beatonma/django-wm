8a99c6c39d800ed14aee7c577e162ca24de3ec9e
Michael Beaton <beatonma@gmail.com>
2019-03-30 16:44:16 +0000

    Reorganised utility constants/functions/etc
    Signed-off-by: Michael Beaton <beatonma@gmail.com>

--- tests/test_url_model_resolution.py
+++ tests/test_url_model_resolution.py
@@ -6,9 +6,9 @@ from django.urls import re_path
 from mentions.exceptions import BadConfig, TargetDoesNotExist
 from mentions.tests import util
 from mentions.tests.models import MentionableTestModel
-from mentions.tests.views.mentionable import AllEndpointsMentionableTestView
-from mentions.tests import constants
-from mentions.tests.constants import views
+from mentions.tests.util import constants
+from mentions.tests.util.urls import urlpatterns as correct_urlpatterns
+from mentions.tests.views import AllEndpointsMentionableTestView
 from mentions.urls import urlpatterns
 from mentions.util import get_model_for_url
 
@@ -19,23 +19,21 @@ bad_modelname_key = 'with_bad_kwarg_key'
 bad_modelname_value = 'with_bad_kwarg_value'
 bad_path = 'some_bad_path'
 
-urlpatterns += util.correct_urlpatterns + [
+urlpatterns += correct_urlpatterns + [
     re_path(
-        fr'^{bad_modelname_key}/{views.all_endpoints}/'
-        fr'(?P<slug>[\w\-.]+)/?$',
+        fr'^{bad_modelname_key}/{constants.slug_regex}',
         AllEndpointsMentionableTestView.as_view(),
         kwargs={
-            'model_name_with_mistyped_key': 'tests.MentionableTestStub',
+            'model_name_with_mistyped_key': constants.model_name,
         },
-        name=f'bad_modelname_key_{views.all_endpoints}'),
+        name=f'bad_modelname_key_{constants.view_all_endpoints}'),
     re_path(
-        fr'^{bad_modelname_value}/{views.all_endpoints}/'
-        fr'(?P<slug>[\w\-.]+)/?$',
+        fr'^{bad_modelname_value}/{constants.slug_regex}',
         AllEndpointsMentionableTestView.as_view(),
         kwargs={
             'model_name': 'tests.UnresolvableModel',
         },
-        name=f'bad_modelname_value_{views.all_endpoints}'),
+        name=f'bad_modelname_value_{constants.view_all_endpoints}'),
 ]
 
 
@@ -53,36 +51,27 @@ class GetModelForUrlTests(TestCase):
     def test_get_model_for_url__with_unknown_url(self):
         """Ensure that URL with an unrecognised path raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
-            get_model_for_url(util.url(views.all_endpoints, bad_path))
+            get_model_for_url(util.url(constants.view_all_endpoints, bad_path))
 
     def test_get_model_for_url__with_correct_slug(self):
         """Ensure that reverse url lookup finds the correct object."""
-        retrieved_object = get_model_for_url(
-            util.url(constants.correct_config, views.all_endpoints, self.slug))
+        retrieved_object = get_model_for_url(util.url(constants.correct_config, self.slug))
 
         self.assertEqual(retrieved_object.stub_id, self.stub_id)
 
     def test_get_model_for_url__with_unknown_slug(self):
         """Ensure that an unknown slug raises an exception."""
         with self.assertRaises(TargetDoesNotExist):
-            get_model_for_url(
-                util.url(constants.correct_config,
-                         views.all_endpoints,
-                         'unknown-slug'))
+            get_model_for_url(util.url(constants.correct_config, 'unknown-slug'))
 
     def test_get_model_for_url__with_bad_model_name_config(self):
-        """
-        Ensure urlpatterns is set up correctly.
-        urlpatterns must provide a correct dotted path value
-        for `model_name` in its kwargs.
-        """
+        """Ensure urlpatterns is set up correctly. urlpatterns must provide a correct dotted path
+        value for `model_name` in its kwargs."""
 
         # Raise BadConfig if kwargs does not have a key named `model_name`.
         with self.assertRaises(BadConfig):
-            get_model_for_url(
-                util.url(bad_modelname_key, views.all_endpoints, self.slug))
+            get_model_for_url(util.url(bad_modelname_key, self.slug))
 
         # Raise BadConfig if `model_name` kwarg has bad/unresolvable path.
         with self.assertRaises(BadConfig):
-            get_model_for_url(
-                util.url(bad_modelname_value, views.all_endpoints, self.slug))
+            get_model_for_url(util.url(bad_modelname_value, self.slug))
