d626a0bc21881b64586a7198ccc2b268ff2c76b8
Michael Beaton <beatonma@gmail.com>
2019-03-30 16:54:16 +0000

    Webmention endpoint is now built from settings.DOMAIN_NAME and settings.WEBMENTION_NAMESPACE - no longer hard-coded.
    Signed-off-by: Michael Beaton <beatonma@gmail.com>

--- middleware/webmention_head_middleware.py
+++ middleware/webmention_head_middleware.py
@@ -2,13 +2,14 @@ from django.conf import settings
 
 
 class WebmentionHeadMiddleware:
+    """Automatically add webmention endpoint to HTTP headers of any HttpResponse
+    objects created by this server."""
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        # response['Link'] = '<https://beatonma.org/webmention>;rel="webmention"'
         response['Link'] = (
-            '<{}/webmention>;rel="webmention"'
-            .format(settings.DOMAIN_NAME))
+            f'<{settings.DOMAIN_NAME}/{settings.WEBMENTION_NAMESPACE}>'
+            ';rel="webmention"')
         return response
