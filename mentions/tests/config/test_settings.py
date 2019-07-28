6793cf289da04efcb25b84b4350fdc89e0e8f60f
Michael Beaton <beatonma@gmail.com>
2019-07-28 15:38:21 +0100

    Trying postgresql instead of sqlite3

--- mentions/tests/config/test_settings.py
+++ mentions/tests/config/test_settings.py
@@ -33,8 +33,12 @@ MIDDLEWARE = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-wm',
+        'USER': 'django-wm',
+        'PASSWORD': 'django-wm-tests',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
