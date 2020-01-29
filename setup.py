c2b6caa81741b3875114c387685d72b43bb9c2ab
Michael Beaton <beatonma@gmail.com>
2020-01-29 22:15:29 +0000

    Added Python 3.8 to classifiers list.

--- setup.py
+++ setup.py
@@ -40,5 +40,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
