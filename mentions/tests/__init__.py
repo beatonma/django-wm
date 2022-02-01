033c0355fadc36cc44146c795dd335652347b231
Michael Beaton <beatonma@gmail.com>
2022-02-01 18:01:34 +0000

    Refactoring for readability.
    Moved EndpointDiscoveryTests to separate file.
    Moved MockResponse to tests.__init__

--- mentions/tests/__init__.py
+++ mentions/tests/__init__.py
@@ -18,3 +18,19 @@ class WebmentionTestCase(TestCase):
             SimpleMention,
         ]:
             Model.objects.all().delete()
+
+
+class MockResponse:
+    """Mock of requests.Response."""
+
+    def __init__(
+        self,
+        url: str,
+        headers: dict = None,
+        text: str = None,
+        status_code: int = None,
+    ):
+        self.url = url
+        self.text = text
+        self.status_code = status_code
+        self.headers = headers
