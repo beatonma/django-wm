c9fbea72a56f3239576bab9104ae354c28d77850
Michael Beaton <beatonma@gmail.com>
2022-02-01 17:41:03 +0000

    Refactoring for readability.
    - Reduced verbosity of mocking decorations.
    - Randomised target website URLs at test runtime to ensure correct mocking behaviour.

--- mentions/tests/test_outgoing_webmention.py
+++ mentions/tests/test_outgoing_webmention.py
@@ -2,32 +2,33 @@
 Tests for webmentions that originate on our server, usually pointing somewhere else.
 """
 import logging
-from unittest import mock
+from unittest.mock import Mock, patch
 
 import requests
-from django.urls import reverse
+from django.conf import settings
 
 from mentions.models import OutgoingWebmentionStatus
 from mentions.tasks import outgoing_webmentions, process_outgoing_webmentions
 from mentions.tests import WebmentionTestCase
-from mentions.tests.models import MentionableTestModel
-from mentions.tests.util import constants, snippets, testfunc, viewname
+from mentions.tests.util import snippets, testfunc
 
 log = logging.getLogger(__name__)
 
 
-OUTGOING_WEBMENTION_HTML = """<html>
+TARGET_DOMAIN = testfunc.random_domain()
+
+OUTGOING_WEBMENTION_HTML = f"""<html>
 <head><link rel="webmention" href="/webmention/" /></head>
 <body>blah blah 
-<a href="https://beatonma.org/">This is a mentionable link</a> 
+<a href="https://{TARGET_DOMAIN}/">This is a mentionable link</a> 
 blah blah</body></html>
 """
 
-OUTGOING_WEBMENTION_HTML_MULTIPLE_LINKS = """<html>
+OUTGOING_WEBMENTION_HTML_MULTIPLE_LINKS = f"""<html>
 <head><link rel="webmention" href="/webmention/" /></head>
 <body>blah blah 
-<a href="https://beatonma.org/">This is a mentionable link</a> 
-<a href="https://beatonma.org/some-article/">This is another mentionable link</a> 
+<a href="https://{TARGET_DOMAIN}/">This is a mentionable link</a> 
+<a href="https://{TARGET_DOMAIN}/some-article/">This is another mentionable link</a> 
 blah blah</body></html>
 """
 
@@ -36,7 +37,7 @@ OUTGOING_WEBMENTION_HTML_NO_LINKS = """<html>
 <body>blah blah no links here blah blah</body></html>
 """
 
-OUTGOING_WEBMENTION_HTML_DUPLICATE_LINKS = """<html>
+OUTGOING_WEBMENTION_HTML_DUPLICATE_LINKS = f"""<html>
 <head><link rel="webmention" href="/webmention/" /></head>
 <body>blah blah 
 <a href="https://beatonma.org/">This is a mentionable link</a> 
@@ -56,175 +57,138 @@ class MockResponse:
         headers: dict = None,
         text: str = None,
         status_code: int = None,
-        *args,
-        **kwargs,
     ):
-        if headers is None:
-            headers = {}
-
         self.url = url
         self.text = text
         self.status_code = status_code
         self.headers = headers
 
-        if args:
-            print(f"Unhandled args: {args}")
-            self.args = args
 
-        if kwargs:
-            print(f"Unhandled kwargs: {kwargs}")
-            self.kwargs = kwargs
-
-
-def _get_mock_get_response_ok(url, *args, **kwargs):
+def _mock_get_ok(url, headers=None, **kwargs):
     return {
-        "https://beatonma.org/": MockResponse(
-            "https://beatonma.org/",
+        f"https://{TARGET_DOMAIN}/": MockResponse(
+            url,
             text=OUTGOING_WEBMENTION_HTML,
-            headers=kwargs.get("headers", {}),
+            headers=headers,
             status_code=200,
         ),
-        "https://beatonma.org/some-article/": MockResponse(
-            "https://beatonma.org/some-article/",
+        f"https://{TARGET_DOMAIN}/some-article/": MockResponse(
+            url,
             text=OUTGOING_WEBMENTION_HTML_MULTIPLE_LINKS,
-            headers=kwargs.get("headers", {}),
+            headers=headers,
             status_code=200,
         ),
     }.get(url)
 
 
-def _get_mock_post_response_ok(url, *args, **kwargs):
+def _mock_post_ok(url, headers=None, **kwargs):
     return {
-        "https://beatonma.org/webmention/": MockResponse(
-            "https://beatonma.org/webmention/",
-            headers=kwargs.get("headers", {}),
+        f"https://{TARGET_DOMAIN}/webmention/": MockResponse(
+            url,
+            headers=headers,
             status_code=200,
         ),
     }.get(url)
 
 
-def _get_mock_post_response_endpoint_error(url, *args, **kwargs):
+def _mock_post_error(url, headers=None, **kwargs):
     return {
-        "https://beatonma.org/webmention/": MockResponse(
-            "https://beatonma.org/webmention/",
+        f"https://{TARGET_DOMAIN}/webmention/": MockResponse(
+            url,
             text=OUTGOING_WEBMENTION_HTML,
-            headers=kwargs.get("headers", {}),
+            headers=headers,
             status_code=400,
         ),
     }.get(url)
 
 
-class OutgoingWebmentionsTests(WebmentionTestCase):
+def _patch_get(ok: bool):
+    return patch.object(requests, "get", Mock(side_effect=_mock_get_ok if ok else None))
+
+
+def _patch_post(ok: bool):
+    return patch.object(
+        requests, "post", Mock(side_effect=_mock_post_ok if ok else _mock_post_error)
+    )
+
+
+class EndpointDiscoveryTests(WebmentionTestCase):
+    """Outgoing webmentions: Endpoint discovery & resolution"""
+
+    absolute_endpoint = testfunc.endpoint_submit_webmention_absolute()
+    relative_endpoint = testfunc.endpoint_submit_webmention()
+
     def setUp(self):
         target_pk, self.target_slug = testfunc.get_id_and_slug()
 
-        MentionableTestModel.objects.create(
-            pk=target_pk,
-            slug=self.target_slug,
-            allow_incoming_webmentions=True,
-        )
-
-    def _get_target_url(self, viewname):
-        return reverse(viewname, args=[self.target_slug])
-
-    def _get_absolute_target_url(self, viewname):
-        return f"https://{constants.domain}{self._get_target_url(viewname)}"
-
-    def test_find_links_in_text(self):
-        """Ensure outgoing links are found correctly."""
-
-        outgoing_content = testfunc.get_mentioning_content(
-            reverse(viewname.with_all_endpoints, args=[self.target_slug])
-        )
-
-        outgoing_links = outgoing_webmentions._find_links_in_text(outgoing_content)
-        self.assertSetEqual(
-            outgoing_links,
-            {testfunc.url_path(constants.correct_config, self.target_slug)},
-        )
-
-    def test_find_links_in_text__should_remove_duplicates(self):
-        outgoing_links = outgoing_webmentions._find_links_in_text(
-            OUTGOING_WEBMENTION_HTML_DUPLICATE_LINKS
-        )
-
-        self.assertSetEqual(
-            {"https://beatonma.org/", "https://snommoc.org/"}, outgoing_links
-        )
+    def _get_absolute_target_url(self):
+        return testfunc.get_url(self.target_slug)
 
     def test_get_absolute_endpoint_from_response(self):
-        """Ensure that any exposed endpoints are found and returned as an absolute url."""
+        """Any exposed endpoints (in HTTP header, HTML <head> or <body>) are found and returned as an absolute url."""
         mock_response = MockResponse(
-            url=self._get_absolute_target_url(viewname.with_all_endpoints),
+            url=self._get_absolute_target_url(),
             headers={"Link": snippets.http_link_endpoint()},
         )
         absolute_endpoint_from_http_headers = (
             outgoing_webmentions._get_absolute_endpoint_from_response(mock_response)
         )
-        self.assertEqual(
-            constants.webmention_api_absolute_url, absolute_endpoint_from_http_headers
-        )
+        self.assertEqual(self.absolute_endpoint, absolute_endpoint_from_http_headers)
 
         mock_response.headers = {}
         mock_response.text = snippets.html_head_endpoint()
         absolute_endpoint_from_html_head = (
             outgoing_webmentions._get_absolute_endpoint_from_response(mock_response)
         )
-        self.assertEqual(
-            constants.webmention_api_absolute_url, absolute_endpoint_from_html_head
-        )
+        self.assertEqual(self.absolute_endpoint, absolute_endpoint_from_html_head)
 
         mock_response.headers = {}
         mock_response.text = snippets.html_body_endpoint()
         absolute_endpoint_from_html_body = (
             outgoing_webmentions._get_absolute_endpoint_from_response(mock_response)
         )
-        self.assertEqual(
-            constants.webmention_api_absolute_url, absolute_endpoint_from_html_body
-        )
+        self.assertEqual(self.absolute_endpoint, absolute_endpoint_from_html_body)
 
     def test_get_endpoint_in_http_headers(self):
-        """Ensure that endpoints exposed in http header are found correctly."""
+        """Endpoints exposed in HTTP header are found correctly."""
 
         mock_response = MockResponse(
-            url=self._get_absolute_target_url(viewname.with_all_endpoints),
+            url=self._get_absolute_target_url(),
             headers={"Link": snippets.http_link_endpoint()},
         )
         endpoint_from_http_headers = outgoing_webmentions._get_endpoint_in_http_headers(
             mock_response
         )
-        self.assertEqual(
-            constants.webmention_api_relative_url, endpoint_from_http_headers
-        )
+        self.assertEqual(self.relative_endpoint, endpoint_from_http_headers)
 
     def test_get_endpoint_in_html_head(self):
-        """Ensure that endpoints exposed in html <head> are found correctly."""
+        """Endpoints exposed in HTML <head> are found correctly."""
 
         mock_response = MockResponse(
-            url=self._get_absolute_target_url(viewname.with_all_endpoints),
+            url=self._get_absolute_target_url(),
             text=snippets.html_head_endpoint(),
         )
         endpoint_from_html_head = outgoing_webmentions._get_endpoint_in_html(
             mock_response
         )
-        self.assertEqual(constants.webmention_api_relative_url, endpoint_from_html_head)
+        self.assertEqual(self.relative_endpoint, endpoint_from_html_head)
 
     def test_get_endpoint_in_html_body(self):
-        """Ensure that endpoints exposed in html <body> are found correctly."""
+        """Endpoints exposed in HTML <body> are found correctly."""
 
         mock_response = MockResponse(
-            url=self._get_absolute_target_url(viewname.with_all_endpoints),
+            url=self._get_absolute_target_url(),
             text=snippets.html_body_endpoint(),
         )
         endpoint_from_html_body = outgoing_webmentions._get_endpoint_in_html(
             mock_response
         )
-        self.assertEqual(constants.webmention_api_relative_url, endpoint_from_html_body)
+        self.assertEqual(self.relative_endpoint, endpoint_from_html_body)
 
     def test_relative_to_absolute_url(self):
-        """Ensure that relative urls are correctly converted to absolute"""
+        """Relative URLs are correctly converted to absolute URLs."""
 
-        domain = constants.domain
+        domain = settings.DOMAIN_NAME
         page_url = f"https://{domain}/some-url-path"
         response = MockResponse(url=page_url)
 
@@ -245,113 +209,106 @@ class OutgoingWebmentionsTests(WebmentionTestCase):
             f"https://{domain}/already_absolute_path", already_absolute_url
         )
 
-    @mock.patch.object(
-        requests,
-        "post",
-        mock.Mock(side_effect=_get_mock_post_response_ok),
-    )
+    def test_find_links_in_text(self):
+        """Outgoing links are found correctly."""
+
+        urls = {
+            f"https://{testfunc.random_domain()}",
+            f"http://{testfunc.random_domain()}/some-path",
+            f"{testfunc.random_domain()}/some-path/something_else_04/",
+            f"https://subdomain.{testfunc.random_domain()}/blah-blah/",
+        }
+
+        outgoing_content = "".join(
+            [
+                f'This is some content that mentions <a href="{url}">this page</a>'
+                for url in urls
+            ]
+        )
+
+        outgoing_links = outgoing_webmentions._find_links_in_text(outgoing_content)
+        self.assertSetEqual(outgoing_links, urls)
+
+    def test_find_links_in_text__should_remove_duplicates(self):
+        outgoing_links = outgoing_webmentions._find_links_in_text(
+            OUTGOING_WEBMENTION_HTML_DUPLICATE_LINKS
+        )
+
+        self.assertSetEqual(
+            {"https://beatonma.org/", "https://snommoc.org/"}, outgoing_links
+        )
+
+
+class OutgoingWebmentionsTests(WebmentionTestCase):
+    source_url = f"https://{settings.DOMAIN_NAME}/some-url-path/"
+
+    @_patch_post(ok=True)
     def test_send_webmention(self):
         """_send_webmention should return True with status code when webmention is accepted by server."""
-        page_url = f"https://{constants.domain}/some-url-path/"
+
         success, status_code = outgoing_webmentions._send_webmention(
-            source_url=page_url,
-            endpoint="https://beatonma.org/webmention/",
-            target="https://beatonma.org/",
+            source_url=self.source_url,
+            endpoint=f"https://{TARGET_DOMAIN}/webmention/",
+            target=f"https://{TARGET_DOMAIN}/",
         )
+
         self.assertTrue(success)
         self.assertEqual(200, status_code)
 
-    @mock.patch.object(
-        requests,
-        "post",
-        mock.Mock(side_effect=_get_mock_post_response_endpoint_error),
-    )
+    @_patch_post(ok=False)
     def test_send_webmention__with_endpoint_error(self):
         """_send_webmention should return False with status code when webmention is not accepted by server."""
-        page_url = f"https://{constants.domain}/some-url-path/"
+
         success, status_code = outgoing_webmentions._send_webmention(
-            source_url=page_url,
-            endpoint="https://beatonma.org/webmention/",
-            target="https://beatonma.org/",
+            source_url=self.source_url,
+            endpoint=f"https://{TARGET_DOMAIN}/webmention/",
+            target=f"https://{TARGET_DOMAIN}/",
         )
+
         self.assertFalse(success)
         self.assertEqual(400, status_code)
 
-    @mock.patch.object(
-        requests,
-        "get",
-        mock.Mock(side_effect=_get_mock_get_response_ok),
-    )
-    @mock.patch.object(
-        requests,
-        "post",
-        mock.Mock(side_effect=_get_mock_post_response_ok),
-    )
+    @_patch_post(ok=True)
+    @_patch_get(ok=True)
     def test_process_outgoing_webmentions(self):
         """Test the entire process_outgoing_webmentions task with no errors."""
         self.assertEqual(0, OutgoingWebmentionStatus.objects.count())
-        page_url = f"https://{constants.domain}/some-url-path/"
-        page_text = OUTGOING_WEBMENTION_HTML
 
-        wm = process_outgoing_webmentions(page_url, page_text)
+        successful_submissions = process_outgoing_webmentions(
+            self.source_url, OUTGOING_WEBMENTION_HTML
+        )
 
-        self.assertEqual(1, wm)
+        self.assertEqual(1, successful_submissions)
         self.assertEqual(1, OutgoingWebmentionStatus.objects.count())
 
-        page_text = OUTGOING_WEBMENTION_HTML_MULTIPLE_LINKS
-        wm = process_outgoing_webmentions(page_url, page_text)
+        successful_submissions = process_outgoing_webmentions(
+            self.source_url, OUTGOING_WEBMENTION_HTML_MULTIPLE_LINKS
+        )
 
-        self.assertEqual(2, wm)
+        self.assertEqual(2, successful_submissions)
         self.assertEqual(3, OutgoingWebmentionStatus.objects.count())
 
-    @mock.patch.object(
-        requests,
-        "get",
-        mock.Mock(
-            return_value=None  # No network requests should be made if links not found in text
-        ),
-    )
-    @mock.patch.object(
-        requests,
-        "post",
-        mock.Mock(
-            return_value=None,  # No network requests should be made if links not found in text
-        ),
-    )
+    # No network requests should be made if links not found in text
+    @patch.object(requests, "get", None)
+    @patch.object(requests, "post", None)
     def test_process_outgoing_webmentions__with_no_links_found(self):
         """Test the entire process_outgoing_webmentions task with no links in provided text."""
         self.assertEqual(0, OutgoingWebmentionStatus.objects.count())
-        page_url = f"https://{constants.domain}/some-url-path/"
-        page_text = OUTGOING_WEBMENTION_HTML_NO_LINKS
 
         successful_webmention_submissions = process_outgoing_webmentions(
-            page_url, page_text
+            self.source_url, OUTGOING_WEBMENTION_HTML_NO_LINKS
         )
 
         self.assertEqual(0, successful_webmention_submissions)
         self.assertEqual(0, OutgoingWebmentionStatus.objects.count())
 
-    @mock.patch.object(
-        requests,
-        "get",
-        mock.Mock(
-            side_effect=_get_mock_get_response_ok,
-        ),
-    )
-    @mock.patch.object(
-        requests,
-        "post",
-        mock.Mock(
-            side_effect=_get_mock_post_response_endpoint_error,
-        ),
-    )
+    @_patch_get(ok=True)
+    @_patch_post(ok=False)
     def test_process_outgoing_webmentions__with_endpoint_error(self):
         """Test the entire process_outgoing_webmentions task with endpoint error."""
-        page_url = f"https://{constants.domain}/some-url-path/"
-        page_text = OUTGOING_WEBMENTION_HTML
 
         successful_webmention_submissions = process_outgoing_webmentions(
-            page_url, page_text
+            self.source_url, OUTGOING_WEBMENTION_HTML
         )
 
         self.assertEqual(0, successful_webmention_submissions)
