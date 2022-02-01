048719f60679ed7c82a91b25f885ef86a8f2d288
Michael Beaton <beatonma@gmail.com>
2022-02-01 18:04:15 +0000

    Added script for generating migrations without a wrapper project.

---
+++ makemigrations.py
@@ -0,0 +1,24 @@
+"""Generate migrations for mentions app without installing it in a project."""
+
+import sys
+
+import django
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+MIGRATION_SETTINGS = {
+    "DEBUG": False,
+    "SECRET_KEY": "django-wm-fake-key",
+    "INSTALLED_APPS": [
+        "django.contrib.contenttypes",
+        "mentions",
+    ],
+    "DEFAULT_AUTO_FIELD": "django.db.models.BigAutoField",
+}
+
+if __name__ == "__main__":
+    settings.configure(**MIGRATION_SETTINGS)
+    django.setup()
+
+    args = sys.argv + ["makemigrations", "mentions"]
+    execute_from_command_line(args)
