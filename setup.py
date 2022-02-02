d612ebfd161699e677215be6b318e7b0201fb2d2
Michael Beaton <beatonma@gmail.com>
2022-02-02 14:32:23 +0000

    Bumped minimum Python version to 3.7 as required by Celery 5.2.
    Replaced minimum >= versions with compatible ~= versions.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "mf2py",
         "requests",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     tests_require=[
         "django-nose",
         "pinocchio",
@@ -46,8 +46,9 @@ setup(
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
