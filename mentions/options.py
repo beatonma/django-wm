79b7f1972174815ff284bb9842da2e43a03baa52
Michael Beaton <beatonma@gmail.com>
2022-08-03 19:41:32 +0100

    Added WEBMENTIONS_URL_SCHEME, retrieved via options.url_scheme().
    This is used to define the scheme of the `source` url when sending a
    webmention. This is intended to allow http calls while debugging in a
    playground setting and should not be used IRL.

--- mentions/options.py
+++ mentions/options.py
@@ -1,21 +1,66 @@
+import logging
+
 from django.conf import settings
+
+NAMESPACE = "WEBMENTIONS"
+SETTING_USE_CELERY = f"{NAMESPACE}_USE_CELERY"
+SETTING_AUTO_APPROVE = f"{NAMESPACE}_AUTO_APPROVE"
+SETTING_URL_SCHEME = f"{NAMESPACE}_URL_SCHEME"
+SETTING_DOMAIN_NAME = "DOMAIN_NAME"
+
+DEFAULTS = {
+    SETTING_DOMAIN_NAME: None,
+    SETTING_AUTO_APPROVE: False,
+    SETTING_URL_SCHEME: "https",
+    SETTING_USE_CELERY: True,
+}
+
+
+log = logging.getLogger(__name__)
+
+
+def _get_attr(key: str):
+    return getattr(settings, key, DEFAULTS[key])
+
+
+def get_config() -> dict:
+    return {key: _get_attr(key) for key in DEFAULTS.keys()}
 
 
 def use_celery() -> bool:
+
     """Return settings.WEBMENTIONS_USE_CELERY, or True if not set.
 
     This setting enables/disables the use of `celery` for running tasks.
     If disabled, user must run these tasks using `manage.py pending_mentions` management command."""
-    return getattr(settings, "WEBMENTIONS_USE_CELERY", True)
+    return _get_attr(SETTING_USE_CELERY)
 
 
 def auto_approve() -> bool:
     """Return settings.WEBMENTIONS_AUTO_APPROVE, or False if not set.
 
-    If True, any received Webmentions will immediately become 'public':they will be included in `/get` API responses.
+    If True, any received Webmentions will immediately become 'public': they will be included in `/get` API responses.
     If False, received Webmentions must be approved by a user with `approve_webmention` permissions.
     """
+    return _get_attr(SETTING_AUTO_APPROVE)
 
-    if hasattr(settings, "WEBMENTIONS_AUTO_APPROVE"):
-        return settings.WEBMENTIONS_AUTO_APPROVE
-    return False
+
+def url_scheme() -> str:
+    """Return settings.WEBMENTIONS_URL_SCHEME.
+
+    This defaults to `https` which is hopefully what your server is using.
+    It's handy to be able to choose when debugging stuff though."""
+    scheme = _get_attr(SETTING_URL_SCHEME)
+    if not settings.DEBUG and scheme == "https":
+        log.warning(
+            f"settings.{SETTING_URL_SCHEME} should not be `http` when in production!"
+        )
+
+    return scheme
+
+
+__all__ = [
+    "use_celery",
+    "auto_approve",
+    "url_scheme",
+]
