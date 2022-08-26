5b180279ee0e37c9aab9192592c3784e8bab5347
Michael Beaton <beatonma@gmail.com>
2022-08-26 13:10:26 +0100

    Consolidated template tags.
    `{% load webmentions %}` now makes `{% webmention_endpoint %}` and
    `{% webmentions_dashboard %}` accessible.

--- mentions/templatetags/webmention_endpoint.py
+++ mentions/templatetags/webmention_endpoint.py
@@ -1,23 +1,15 @@
 from django import template
-from django.urls import reverse
-from django.utils.html import format_html
 
-from mentions.views import view_names
+from .webmentions import webmentions_endpoint
+
+__all__ = [
+    "webmentions_endpoint",
+]
 
 register = template.Library()
 
 
 @register.simple_tag
 def webmention_endpoint():
-    """Add your webmentions endpoint to your template <head>.
-
-    e.g.
-    {% load webmention_endpoint %}
-    ...
-    <head>
-        {% webmention_endpoint %}
-    </head>
-    ...
-    """
-    endpoint = reverse(view_names.webmention_api_incoming)
-    return format_html("""<link rel="webmention" href="{}" />""", endpoint)
+    """Implementation moved but kept available here to avoid breaking change (for now)."""
+    return webmentions_endpoint()
