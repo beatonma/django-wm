78d933e49bd14ca8743a305cdee18d04b938dd5c
Michael Beaton <beatonma@gmail.com>
2022-09-29 18:15:34 +0100

    Fixed logic on scheme warning log message.

--- mentions/options.py
+++ mentions/options.py
@@ -113,7 +113,7 @@ def url_scheme() -> str:
     This defaults to `https` which is hopefully what your server is using.
     It's handy to be able to choose when debugging stuff though."""
     scheme = _get_attr(SETTING_URL_SCHEME)
-    if not settings.DEBUG and scheme == "https":
+    if not settings.DEBUG and scheme != "https":
         log.warning(
             f"settings.{SETTING_URL_SCHEME} should not be `http` when in production!"
         )
