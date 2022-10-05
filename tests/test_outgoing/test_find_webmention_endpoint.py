746425bd6ad46cb998e2731367bb0df1dc53d36d
Michael Beaton <beatonma@gmail.com>
2022-10-05 16:22:11 +0100

    Additional test for HTTP header link resolution.

--- tests/test_outgoing/test_find_webmention_endpoint.py
+++ tests/test_outgoing/test_find_webmention_endpoint.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from requests.structures import CaseInsensitiveDict
 
 from mentions.tasks.outgoing.parsing.webmention_endpoint import (
@@ -7,59 +9,78 @@ from tests import WebmentionTestCase
 from tests.util import snippets, testfunc
 
 
-class FindWebmentionEndpointTests(WebmentionTestCase):
-    def test_find_endpoint_in_http_headers(self):
-        headers = CaseInsensitiveDict(
+def _build_headers(link: Union[list[str], str]) -> CaseInsensitiveDict:
+    if isinstance(link, str):
+        return CaseInsensitiveDict(
             {
-                "Link": snippets.http_link_endpoint(),
+                "Link": link,
             }
         )
+
+    return CaseInsensitiveDict(
+        {
+            "Link": ", ".join(link),
+        }
+    )
+
+
+class FindWebmentionEndpointHttpHeadersTests(WebmentionTestCase):
+    def test_find_endpoint_in_http_headers(self):
+        headers = _build_headers(snippets.http_link_endpoint())
 
         self.assertEqual(
             testfunc.endpoint_submit_webmention(),
             get_endpoint_in_http_headers(headers),
         )
 
+    def test_find_endpoint_in_http_headers_with_additional_attrs(self):
+        headers = _build_headers(
+            snippets.http_header_link(
+                "https://endpoint.org",
+                title="Title",
+                rel="webmention",
+                hreflang="en-GB",
+            )
+        )
+
+        self.assertEqual("https://endpoint.org", get_endpoint_in_http_headers(headers))
+
     def test_find_endpoint_in_http_headers_with_many_links(self):
-        headers = CaseInsensitiveDict(
-            {
-                "Link": ", ".join(
-                    [
-                        snippets.http_header_link(
-                            testfunc.random_url(),
-                            **{
-                                "rel": "preload",
-                                "as": "script",
-                                "nopush": None,
-                            },
-                        ),
-                        snippets.http_header_link(
-                            "https://websub.io",
-                            rel="websub",
-                        ),
-                        snippets.http_header_link(
-                            testfunc.random_url(),
-                            **{
-                                "rel": "preload",
-                                "as": "script",
-                                "nopush": None,
-                            },
-                        ),
-                        snippets.http_header_link(
-                            "https://webmention.io",
-                            rel="webmention",
-                        ),
-                        snippets.http_header_link(
-                            testfunc.random_url(),
-                            **{
-                                "rel": "preload",
-                                "as": "script",
-                                "nopush": None,
-                            },
-                        ),
-                    ]
+        headers = _build_headers(
+            [
+                snippets.http_header_link(
+                    testfunc.random_url(),
+                    **{
+                        "rel": "preload",
+                        "as": "script",
+                        "nopush": None,
+                    },
                 ),
-            }
+                snippets.http_header_link(
+                    "https://websub.io",
+                    rel="websub",
+                ),
+                snippets.http_header_link(
+                    testfunc.random_url(),
+                    **{
+                        "rel": "preload",
+                        "as": "script",
+                        "nopush": None,
+                    },
+                ),
+                snippets.http_header_link(
+                    "https://webmention.io",
+                    rel="webmention",
+                ),
+                snippets.http_header_link(
+                    testfunc.random_url(),
+                    **{
+                        "rel": "preload",
+                        "as": "script",
+                        "nopush": None,
+                    },
+                ),
+            ]
         )
 
         self.assertEqual(
