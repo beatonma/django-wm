8ebb05702d136b992e82e0c1f83894a503a99a98
Michael Beaton <beatonma@gmail.com>
2022-10-06 16:49:52 +0100

    Simplified mocking with standard `patch_http_post`, `patch_http_get`.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Callable, Optional
 from unittest.mock import Mock, patch
 
@@ -7,6 +8,8 @@ from django.test import TestCase
 from requests.structures import CaseInsensitiveDict
 
 from mentions import options
+
+log = logging.getLogger(__name__)
 
 
 class SimpleTestCase(TestCase):
@@ -90,12 +93,6 @@ class OptionsTestCase(WebmentionTestCase):
         setattr(settings, options.SETTING_ALLOW_SELF_MENTIONS, allow)
 
 
-class MetaTestCase(TestCase):
-    """Base testcase class for tests that test test-specific stuff."""
-
-    pass
-
-
 class MockResponse:
     """Mock of requests.Response."""
 
@@ -114,4 +111,56 @@ class MockResponse:
         self.url = url
         self.text = text
         self.status_code = status_code
-        self.headers = CaseInsensitiveDict(headers)
+        self.headers = CaseInsensitiveDict(headers or {"Content-Type": "text/html"})
+        log.debug(self)
+
+    def __str__(self):
+        return f"[{self.status_code}] {self.url}"
+
+
+def patch_http_get(
+    status_code: int = 200,
+    text: Optional[str] = None,
+    headers: Optional[dict] = None,
+    response: Optional[Callable] = None,
+):
+    headers = headers or {"content-type": "text/html"}
+
+    side_effect = (
+        response
+        if response
+        else lambda url, **kw: MockResponse(
+            url,
+            headers=headers,
+            text=text or "",
+            status_code=status_code,
+        )
+    )
+
+    return patch.object(
+        requests,
+        "get",
+        Mock(side_effect=side_effect),
+    )
+
+
+def patch_http_post(
+    status_code: int = 200,
+    headers: Optional[dict] = None,
+    response: Optional[Callable] = None,
+):
+    side_effect = (
+        response
+        if response
+        else lambda url, **kw: MockResponse(
+            url,
+            headers=headers,
+            status_code=status_code,
+        )
+    )
+
+    return patch.object(
+        requests,
+        "post",
+        Mock(side_effect=side_effect),
+    )
