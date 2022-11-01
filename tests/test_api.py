24e2a906f8eab6352975ea07ff4d5c05829d74c5
Michael Beaton <beatonma@gmail.com>
2022-11-01 14:40:30 +0000

    Minor refactor

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,79 +18,97 @@ class ApiTestCase(WebmentionTestCase):
         _module = import_module(self.module_name)
         self.member_names = [x[0] for x in inspect.getmembers(_module)]
 
-    def assertExistsInModule(self, obj_name: str):
-        self.assertTrue(
-            obj_name in self.member_names,
-            msg=f"Object '{obj_name}' not found in module '{self.module_name}'",
-        )
+    def assertExistsInModule(self, *obj_names: str):
+        for obj_name in obj_names:
+            self.assertTrue(
+                obj_name in self.member_names,
+                msg=f"Object '{obj_name}' not found in module '{self.module_name}'",
+            )
 
-    def assertNotInModule(self, obj_name: str):
-        self.assertFalse(
-            obj_name in self.member_names,
-            msg=f"Object '{obj_name}' should not be accessible in module '{self.module_name}'",
-        )
+    def assertNotInModule(self, *obj_names: str):
+        for obj_name in obj_names:
+            self.assertFalse(
+                obj_name in self.member_names,
+                msg=f"Object '{obj_name}' should not be accessible in module '{self.module_name}'",
+            )
 
 
 class FormApiTests(ApiTestCase):
     module_name = "mentions.forms"
 
     def test_expected_forms_are_accessible(self):
-        self.assertExistsInModule("SubmitWebmentionForm")
+        self.assertExistsInModule(
+            "SubmitWebmentionForm",
+        )
 
 
 class HelperApiTests(ApiTestCase):
     module_name = "mentions.helpers"
 
     def test_expected_helpers_are_accessible(self):
-        self.assertExistsInModule("mentions_path")
-        self.assertExistsInModule("mentions_re_path")
+        self.assertExistsInModule(
+            "mentions_path",
+            "mentions_re_path",
+        )
 
 
 class MiddlewareApiTests(ApiTestCase):
     module_name = "mentions.middleware"
 
     def test_expected_middleware_are_accessible(self):
-        self.assertExistsInModule("WebmentionHeadMiddleware")
+        self.assertExistsInModule(
+            "WebmentionHeadMiddleware",
+        )
 
 
 class MixinApiTests(ApiTestCase):
     module_name = "mentions.models.mixins"
 
     def test_expected_mixins_are_accessible(self):
-        self.assertExistsInModule("RetryableMixin")
-        self.assertExistsInModule("MentionableMixin")
-        self.assertExistsInModule("QuotableMixin")
+        self.assertExistsInModule(
+            "RetryableMixin",
+            "MentionableMixin",
+            "QuotableMixin",
+        )
 
 
 class ModelApiTests(ApiTestCase):
     module_name = "mentions.models"
 
     def test_expected_models_are_accessible(self):
-        self.assertExistsInModule("HCard")
-        self.assertExistsInModule("OutgoingWebmentionStatus")
-        self.assertExistsInModule("PendingIncomingWebmention")
-        self.assertExistsInModule("PendingOutgoingContent")
-        self.assertExistsInModule("SimpleMention")
-        self.assertExistsInModule("Webmention")
+        self.assertExistsInModule(
+            "HCard",
+            "OutgoingWebmentionStatus",
+            "PendingIncomingWebmention",
+            "PendingOutgoingContent",
+            "SimpleMention",
+            "Webmention",
+        )
 
-        self.assertNotInModule("RetryableMixin")
-        self.assertNotInModule("MentionableMixin")
-        self.assertNotInModule("QuotableMixin")
+        self.assertNotInModule(
+            "RetryableMixin",
+            "MentionableMixin",
+            "QuotableMixin",
+        )
 
 
 class TaskApiTests(ApiTestCase):
     module_name = "mentions.tasks"
 
     def test_expected_tasks_are_accessible(self):
-        self.assertExistsInModule("handle_pending_webmentions")
-        self.assertExistsInModule("handle_incoming_webmention")
-        self.assertExistsInModule("handle_outgoing_webmentions")
+        self.assertExistsInModule(
+            "handle_pending_webmentions",
+            "handle_incoming_webmention",
+            "handle_outgoing_webmentions",
+        )
 
 
 class ViewApiTests(ApiTestCase):
     module_name = "mentions.views"
 
     def test_expected_views_are_accessible(self):
-        self.assertExistsInModule("GetMentionsView")
-        self.assertExistsInModule("WebmentionView")
-        self.assertExistsInModule("WebmentionDashboardView")
+        self.assertExistsInModule(
+            "GetMentionsView",
+            "WebmentionView",
+            "WebmentionDashboardView",
+        )
