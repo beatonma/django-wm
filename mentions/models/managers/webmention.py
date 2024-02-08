05db13bb9a92483ff981dedb5c7b17c914b2e105
Michael Beaton <beatonma@gmail.com>
2024-02-08 16:49:02 +0000

    Minor tidy-up, broadly grouping methods by purpose.

--- mentions/models/managers/webmention.py
+++ mentions/models/managers/webmention.py
@@ -10,14 +10,14 @@ class WebmentionQuerySet(QuerySet):
     def filter_unread(self) -> "WebmentionQuerySet":
         return self.filter(has_been_read=False)
 
-    def mark_as_read(self) -> int:
-        return self.update(has_been_read=True)
-
-    def mark_as_unread(self) -> int:
-        return self.update(has_been_read=False)
-
     def filter_approved(self) -> "WebmentionQuerySet":
         return self.filter(approved=True)
+
+    def filter_validated(self) -> "WebmentionQuerySet":
+        return self.filter(validated=True)
+
+    def filter_public(self) -> "WebmentionQuerySet":
+        return self.filter_approved().filter_validated()
 
     def mark_as_approved(self) -> int:
         return self.update(approved=True)
@@ -25,8 +25,8 @@ class WebmentionQuerySet(QuerySet):
     def mark_as_unapproved(self) -> int:
         return self.update(approved=False)
 
-    def filter_validated(self) -> "WebmentionQuerySet":
-        return self.filter(validated=True)
+    def mark_as_read(self) -> int:
+        return self.update(has_been_read=True)
 
-    def filter_public(self) -> "WebmentionQuerySet":
-        return self.filter_approved().filter_validated()
+    def mark_as_unread(self) -> int:
+        return self.update(has_been_read=False)
