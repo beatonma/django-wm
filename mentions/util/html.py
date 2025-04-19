153c630c853d060a7857e58a9e9b650ceabc6269
Michael Beaton <beatonma@gmail.com>
2025-04-19 15:29:38 +0100

    Fix: `tailwindcss` utilities no longer break microformat parsing.
    `html_parser` now removes any `h-` CSS class which is not a known
    microformat container class.
    
    Previously, `tailwindcss` height utilities (e.g. `h-full`, `h-[2em]`)
    interfered with `mf2py` parsing.

--- mentions/util/html.py
+++ mentions/util/html.py
@@ -1,4 +1,5 @@
-from typing import List, Set
+import re
+from typing import List
 
 from bs4 import BeautifulSoup, ResultSet, Tag
 
@@ -9,8 +10,10 @@ __all__ = [
 ]
 
 
-def html_parser(content) -> BeautifulSoup:
-    return BeautifulSoup(content, features="html5lib")
+def html_parser(content: str) -> Tag:
+    soup = BeautifulSoup(content, features="html5lib")
+
+    return _clean_soup(soup)
 
 
 def find_links_in_html(html: str) -> List[Tag]:
@@ -18,5 +21,22 @@ def find_links_in_html(html: str) -> List[Tag]:
     return find_links_in_soup(soup)
 
 
-def find_links_in_soup(soup: BeautifulSoup) -> ResultSet:
+def find_links_in_soup(soup: Tag) -> ResultSet:
     return soup.find_all("a", href=True)
+
+
+def _clean_soup(soup: Tag) -> Tag:
+    """Remove any CSS classes that may look like `h-` microformat containers
+    (e.g. `tailwindcss` height utilities) as these interfere with microformat parsing.
+    """
+
+    pattern = re.compile(r"(h-(?!adr|card|entry|feed|geo|cite|event).+)")
+    for tag in soup.find_all(class_=True):
+        classes = tag.get("class")
+        filtered_classes = [c for c in classes if not pattern.match(c)]
+        if filtered_classes:
+            tag["class"] = filtered_classes
+        else:
+            del tag["class"]
+
+    return soup
